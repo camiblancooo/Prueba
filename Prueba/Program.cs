074a96cec5ed3862e80fd6d890414b639be55b1a
Cami Blanco <camiblanco140599@gmail.com>
2026-03-16 11:17:04 -0300

    Un cambio de prueba

--- Prueba/Program.cs
+++ Prueba/Program.cs
@@ -11,6 +11,7 @@ namespace Prueba
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
+            // Comentario de prueba
             Application.Run(new Form1());
         }
     }
